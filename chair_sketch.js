265810a45795f54fcf15799409091a0c84bd7386
Simon Winter <simon.winter@infontology.org>
2020-07-19 07:08:11 +0200

    Fyra dimensioner

--- chair_sketch.js
+++ chair_sketch.js
@@ -3,15 +3,14 @@ var fixed = {x: 400, y:300}; //En fast punkt
 var sitshoejd = 45;
 var rygghoejd = {min: 0, max: 70};
 var sitsvidd = {min: 30, max: 70};
-var ryggvinkel = {min: 0, max: 1.57};
-var lem = {min: 30, max: 100};
+var ryggvinkel = {min: 0, max: Math.PI/2};
+var armstoed = {min: 0, max: rygghoejd};
 
 
 function setup() {
   // create canvas
   createCanvas(710, 400);
-  textSize(15);
-  noStroke();
+
 
   // create sliders
   sitsviddSlider = createSlider(sitsvidd.min, sitsvidd.max, 45);
@@ -20,13 +19,18 @@ function setup() {
   rygghoejdSlider.position(20, 50);
   ryggvinkelSlider = createSlider(ryggvinkel.min, ryggvinkel.max, 0, 0.01);
   ryggvinkelSlider.position(20, 80);
+  armstoedSlider = createSlider(armstoed.min, armstoed.max, 0);
+  armstoedSlider.position(20, 110);
+
 
 }
 
 function draw() {
-  var sitsvidd = sitsviddSlider.value();
+  frameRate(20);
+  sitsvidd = sitsviddSlider.value();
   rygghoejd = rygghoejdSlider.value();
   ryggvinkel = ryggvinkelSlider.value();
+  armstoed = armstoedSlider.value();
   //var lemmar = lSlider.value();
   //var kropp = kSlider.value();
   strokeWeight(7);
@@ -38,11 +42,15 @@ function draw() {
   line(fixed.x-sitsvidd, fixed.y, fixed.x-sitsvidd, fixed.y+sitshoejd); //framben
   line(fixed.x, fixed.y, fixed.x, fixed.y+sitshoejd); //bakben
   line(fixed.x, fixed.y, fixed.x+(rygghoejd*Math.sin(ryggvinkel)), fixed.y-rygghoejd*Math.cos(ryggvinkel)); //rygg
+  line(fixed.x-sitsvidd, fixed.y, fixed.x-sitsvidd, fixed.y-armstoed); //armstoed1 i framben
+  line(fixed.x+armstoed*Math.tan(ryggvinkel), fixed.y-armstoed, fixed.x-sitsvidd, fixed.y-armstoed); //armstoed2 vågrät
 
-  strokeWeight(1);
+  textSize(15);
+  noStroke();
   text("sitsvidd " + sitsviddSlider.value(), sitsviddSlider.x * 2 + sitsviddSlider.width, 35);
   text("rygghoejd " + rygghoejdSlider.value(), rygghoejdSlider.x * 2 + rygghoejdSlider.width, 65);
-  text("ryggvinkel " + ryggvinkelSlider.value(), ryggvinkelSlider.x * 2 + ryggvinkelSlider.width, 95);
+  text("ryggvinkel " + ryggvinkelSlider.value()*180/Math.PI, ryggvinkelSlider.x * 2 + ryggvinkelSlider.width, 95);
+  text("armstöd " + armstoedSlider.value(), armstoedSlider.x * 2 + armstoedSlider.width, 125);
 
 
 }
